aeba5888f493d926f4fc5f2e0293aca1bba7e0ab
JVictorV <joaovictorvisona@gmail.com>
2022-08-07 22:35:07 -0300

    feat: health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+pub mod lib;
+
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 80");
+
+    run(listener)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,31 @@
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::test]
+async fn health_check_works() {
+    let base_address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}/health_check", base_address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
